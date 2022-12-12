9fc516595280f656c2cba3b96885bf2bf7f09e79
Choi-Sunwoong <119837295+Choi-Sunwoong@users.noreply.github.com>
2022-12-12 15:18:24 +0900

    Add files via upload

---
+++ 04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,21 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		ArrayExample1 Run = new ArrayExample1();
+		
+		//Run.ex1();
+		//Run.ex2();
+		//Run.ex3();
+		//Run.ex4();
+		//Run.ex5();
+		//Run.ex6();
+		//Run.ex7();
+		Run.ex8();
+		
+		
+		}
+	}
+
+
