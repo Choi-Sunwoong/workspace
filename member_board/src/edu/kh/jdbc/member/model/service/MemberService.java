222793ed4bef7fda280f08d10558aa6fa7ae496d
Choi-Sunwoong <119837295+Choi-Sunwoong@users.noreply.github.com>
2023-02-09 08:02:58 +0900

    morning

--- member_board/src/edu/kh/jdbc/member/model/service/MemberService.java
+++ member_board/src/edu/kh/jdbc/member/model/service/MemberService.java
@@ -2,4 +2,14 @@ package edu.kh.jdbc.member.model.service;
 
 public class MemberService {
 
+	public static String getAttribute(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String getParameter(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
